18b59b1c3ca83fc8f0b8de7b2ec97d85e4da92a9
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-18 22:05:32 -0400

    add comments to example4.js and relay/index.js

--- chapter_2/example4.js
+++ chapter_2/example4.js
@@ -1,3 +1,3 @@
-var os = require('os');
+var os = require('os'); // using a core module or module specifically for project
 
-console.log('This code is running on: ' + os.type());
+console.log('This code is running on: ' + os.type()); // find out what os the code is running on
--- chapter_2/relay/index.js
+++ chapter_2/relay/index.js
@@ -1,4 +1,4 @@
-prefix = 'Relaying: '; // prefix currently is a global variable
+var prefix = 'Relaying: '; // prefix is a local variable
 
 module.exports = function (message) {
 	console.log(prefix + message);
