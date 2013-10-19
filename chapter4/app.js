bee2681ee45c1a0fb4105d0f59fd879982601034
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-19 01:20:42 -0400

    re-write module for individually scoped instances

--- chapter4/app.js
+++ chapter4/app.js
@@ -1,7 +1,31 @@
-var flight = require('./flight'); // require flight module
+var flight = require('./flight');
 
-flight.setOrigin('LAX');
-flight.setDestination('DCA');
-flight.setNumber(462);
+// define a flight's information by setting properties in an object literal
+// that will eventually be passed into the flight module to give us back a
+// flight
+var pdxlax = {
+	number: 847,
+	origin: 'PDX',
+	destination: 'LAX'
+};
 
-console.log(flight.getInfo());
+// pl contains the functions that the flight module returns
+// i.e., we have a flight!
+var pl = flight(pdxlax);
+
+pl.triggerDepart();
+
+console.log(pl.getInformation());
+
+// defining another flight's informtion
+var ausdca = {
+	number: 382,
+	origin: 'AUS',
+	destination: 'DCA'
+};
+
+var ad = flight(ausdca);
+
+console.log(ad.getInformation());
+
+console.log(pl.getInformation());
--- chapter4/flight/index.js
+++ chapter4/flight/index.js
@@ -1,24 +1,44 @@
-var number, origin, destination;
+// only exporting one function. a new function scope is created each time the
+// function is called
 
-// export functions so it can be used elsewhere
-// setters
-exports.setNumber = function(num) {
-	number = num;
-}
+// pass in an info object, the values of which will be used used to set the
+// values of the values object
+module.exports = function(info) {
 
-exports.setOrigin = function(o) {
-	origin = o;
-}
-
-exports.setDestination = function(d) {
-	destination = d;
-}
-
-// getter
-exports.getInfo = function() {
-	return {
-		number: number,
-		origin: origin,
-		destination: destination
+	// define object for flight values
+	var values = {
+		number: null,
+		origin: null,
+		destination: null,
+		departs: null,
+		arrives: null,
+		actualDepart: null,
+		actualArrive: null
 	};
-}
+
+	// sets values of the values object
+	for(var prop in values) {
+		if(values[prop] !== 'undefined') {
+			values[prop] = info[prop];
+		}
+	}
+
+	// define functions for:
+	// departure time
+	// arrival time
+	// getter for the value object
+	var functions = {
+		triggerDepart: function() {
+			values.actualDepart = Date.now();
+		},
+		triggerArrive: function() {
+			values.actualArrive = Date.now();
+		},
+		getInformation: function() {
+			return values;
+		}
+	};
+
+	return functions; // return the functions object
+
+};
