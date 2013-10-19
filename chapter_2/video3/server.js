9f513702b5e7ccb88235c369b1a87585cc9539ba
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-18 22:18:45 -0400

    add comments to server.js

--- chapter_2/video3/server.js
+++ chapter_2/video3/server.js
@@ -1,10 +1,16 @@
-var http = require('http');
+// keep serving requests, regardless of how many requests come in
 
+var http = require('http'); // require http module
+
+// gets executed whenever a request comes in
+// receives request 'req' and reqponse 'res' objects
 var handleRequest = function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Welcome to Node Essential Training\n');
+	res.writeHead(200, {'Content-Type': 'text/plain'}); // writing http header
+	res.end('Welcome to Node Essential Training\n'); // writing some content
 };
 
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest); // creating a server, passing in hanldeRequest function
 
-server.listen(3000, 'localhost');
+// node starts the server, and keeps listening
+// keeps listening for requests until server is stopped
+server.listen(3000, 'localhost'); // takes port number, address
