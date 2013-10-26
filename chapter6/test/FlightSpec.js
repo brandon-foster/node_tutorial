6327ee9b28ab31e1ccfaaf0504adb86777600cd0
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-25 23:06:24 -0400

    write sample mocha unit tests

--- chapter6/test/FlightSpec.js
+++ chapter6/test/FlightSpec.js
@@ -1,1 +1,18 @@
 var app = require('./helpers/app');
+
+var should = require('should'),
+	supertest = require('supertest');
+	
+// start a unit test in mocha
+// arguments: label for the suite, anonymous function that has individual unit tests
+describe('flights', function() {
+	
+	it('should pass', function(done) {
+		done();
+	});
+	
+	it('should not pass', function(done) {
+		throw "don't pass";
+		done();
+	});
+});
