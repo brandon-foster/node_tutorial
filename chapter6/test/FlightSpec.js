569df50e4598a18efa1883f71fe3256c18bbf807
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-26 12:59:22 -0400

    using supertest to mock HTTP requests

--- chapter6/test/FlightSpec.js
+++ chapter6/test/FlightSpec.js
@@ -7,12 +7,51 @@ var should = require('should'),
 // arguments: label for the suite, anonymous function that has individual unit tests
 describe('flights', function() {
 	
-	it('should pass', function(done) {
-		done();
+	// test get requests to /flight/18
+	it('should return valid flight data for flight 18', function(done) {
+		
+		supertest(app)
+		.get('/flight/18')
+		.expect(200)
+		.end(function(err, res) {
+			res.status.should.equal(200);
+			done();
+		});
 	});
 	
-	it('should not pass', function(done) {
-		throw "don't pass";
-		done();
+	it('should return an error for an invalid flight', function(done) {
+		supertest(app)
+		.get('/flight/999999')
+		.expect(404)
+		.end(function(err, res) {
+			res.status.should.equal(404);
+			done();
+		});
 	});
+	
+	it('should mark a flight as arrived', function(done) {
+		
+		// a PUT request to /flight/[flight_number]/arrived
+		// should send back {"status": "done"}
+		supertest(app)
+		.put('/flight/18/arrived')
+		.expect(200)
+		.end(function(err, res) {
+			res.status.should.equal(200);
+			res.body.status.should.equal('done');
+			
+			// after sending a PUT request to /flight/[flight_number]/arrived, a GET
+			//request to /flight/[flight_number] should respond with a JSON object
+			// containing an an "actualArrive" : "[number]" name-value pair
+			supertest(app)
+			.get('/flight/18')
+			.expect(200)
+			.end(function(err, res) {
+				res.status.should.equal(200);
+				res.body.actualArrive.should.not.equal(undefined);
+				
+				done();
+			})
+		});
+	})
 });
