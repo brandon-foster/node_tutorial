051105b42218a02a5fe7307b97d8503887839998
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-10-26 13:31:48 -0400

    add mongoDB connection

--- chapter7/server.js
+++ chapter7/server.js
@@ -1,6 +1,11 @@
+// new entry point for the application
+// responsible for creating the server
+
 var http = require('http'),
-	flights = require('./data'),
-	app = require('./app')(flights);
+	flights = require('./data'), // require the flights data, which can
+								 // be changed to anything easily in the future
+	app = require('./app')(flights), // pass flights to app.js
+	db = require('./db');// the db module exports the database connection
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
