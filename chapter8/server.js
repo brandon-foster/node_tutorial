0c933c7627b2a281fccf0352702b360cf93eea3c
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-11-08 19:08:19 -0500

    use the repl module and assign the flight data (flights) to its context

--- chapter8/server.js
+++ chapter8/server.js
@@ -1,8 +1,16 @@
 var http = require('http'),
 	flights = require('./data'),
+	repl = require('repl'); // require the repl
 	db = require('./db'),
 	app = require('./app')(flights, db);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// set the prompt for the repl
+var prompt = repl.start({prompt: 'flights> '});
+
+// can use context to set properties that will be used as as global
+// variables in this repl
+prompt.context.data = flights;
