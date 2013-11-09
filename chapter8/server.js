3fb866506683a873a408dcadc844968453af7b90
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-11-08 19:21:13 -0500

    use optimist to use command line property-value pairs [more]
    to set specified destinations on specified flights

--- chapter8/server.js
+++ chapter8/server.js
@@ -1,16 +1,16 @@
 var http = require('http'),
 	flights = require('./data'),
-	repl = require('repl'); // require the repl
 	db = require('./db'),
+	argv = require('optimist').argv, // creates an object that has
+									 //properties and values passed to command line
 	app = require('./app')(flights, db);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// set the prompt for the repl
-var prompt = repl.start({prompt: 'flights> '});
-
-// can use context to set properties that will be used as as global
-// variables in this repl
-prompt.context.data = flights;
+// use command line property-value pairs to set
+// specified destinations on specified flights
+if (argv.flight && argv.destination) {
+  flights[argv.flight].data.destination = argv.destination;
+}
