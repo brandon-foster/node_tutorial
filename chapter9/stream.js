b179036e2f7bd6768c9c874c2b0681ba9a2e1476
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-11-08 20:11:16 -0500

    add comments to explain listeners in stream.js

--- chapter9/stream.js
+++ chapter9/stream.js
@@ -1,18 +1,23 @@
+// core module for dealing with the file system
 var fs = require('fs');
 
+// create a stream out of data.json
+// instead of reading the file at once, reads it one chunk at a time
 var stream = fs.createReadStream('data.json');
 
+// listener to data event, accepting a chunk as an argument
 stream.on('data', function (chunk) {
 	console.log('----------------begin chunk----------------');
-	console.log(chunk.toString());
+	console.log(chunk.toString()); // converts the chunk to a string
 	console.log('----------------end chunk----------------');
 });
 
-
+// listener to data event, accepting a chunk as an argument
 stream.on('data', function (chunk) {
-	console.log('CHUNK LENGTH WAS: ' + chunk.length);
+	console.log('CHUNK LENGTH WAS: ' + chunk.length); // getting lenght of the chunk
 });
 
+// listener fires when end of file is reached
 stream.on('end', function  () {
 	console.log('----------------reached file end----------------');
 });
